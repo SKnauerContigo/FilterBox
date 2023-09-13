549b3de7c25f1d5a17070636bdbdad6bca885ad3
SKnauerContigo <sven.knauer@contigo-consulting.com>
2023-09-13 05:57:44 +0200

    add option for debugging

--- FilterBox.js
+++ FilterBox.js
@@ -8,6 +8,7 @@
     <label for="select_box_filter">Filter:</label>
           <input id='search' type="text" name="search" list="select_box_filter/>
           <select id="select_box_filter">
+            <option></option>
           </select>
           
     <button id="close_button">Close</button>
