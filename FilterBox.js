a1e533e1f8f9c5daf6a65c766951848272a20531
SKnauerContigo <sven.knauer@contigo-consulting.com>
2023-09-13 05:02:36 +0200

    append child debugging

--- FilterBox.js
+++ FilterBox.js
@@ -47,7 +47,7 @@
               if (desc[k].includes(searchterm)) {
                 console.log("found member");
                 opt.value = opt.text = desc[k];
-                memberList.appendChild(opt);
+                memberList.shadowRoot.appendChild(opt);
               }
             }
           }
