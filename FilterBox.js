8aae56d80bab55bcdad044ea5fc4df092b6410eb
SKnauerContigo <sven.knauer@contigo-consulting.com>
2023-09-13 06:39:17 +0200

    datalist instead of select

--- FilterBox.js
+++ FilterBox.js
@@ -7,8 +7,8 @@
     <div class="child" >
     <label for="select_box_filter">Filter:</label>
           <input id="search" type="text" name="search" list="select_box_filter"/>
-          <select id="select_box_filter">
-          </select>          
+          <datalist id="select_box_filter">
+          </datalist>          
     <button id="close_button">Close</button>
     </div>
 
